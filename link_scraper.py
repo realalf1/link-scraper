3e1cdb38f06772ac8eb0d2de9d7a7939008a2687
Alfiyan <152812058+realalf1@users.noreply.github.com>
2024-01-09 17:34:16 +0700

    Update link_scraper.py

--- link_scraper.py
+++ link_scraper.py
@@ -4,30 +4,35 @@ import requests
 class Link:
     def __init__(self, link):
         self.link = link
-    
+
     def get_link(self):
         return self.link
 
     def scraping_link(self):
-        r = requests.get(self.link).text
-        soup = BeautifulSoup(r, 'html.parser')
+        try:
+            r = requests.get(self.link)
+        
+            if r.status_code == 200:
+                soup = BeautifulSoup(r.text, 'html.parser')
 
-        link_unique = set()
-        num = 1
+                link_unique = set()
+                num = 1
 
-        for link in soup.find_all('a'):
-            href = link.get('href')
-            if href not in link_unique:
-                print(f"{num}. {href}")
-                link_unique.add(href)
-                num += 1
+                for link in soup.find_all('a'):
+                    href = link.get('href')
+                    if href not in link_unique:
+                        print(f"{num}. {href}")
+                        link_unique.add(href)
+                        num += 1
+
+        except:
+            print(f"{self.link} Failed to resolved\n")
 
     def run(self):
-        print(f"Link: {self.get_link()}")
-        print("Scraping Link:")
+        print(f"Domain: {self.get_link()}")
         self.scraping_link()
 
 if __name__ == "__main__":
 
-    link = Link("your-link")
+    link = Link("https://link.com")
     link.run()
